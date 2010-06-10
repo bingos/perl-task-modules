f0ece1c9dcfaf1850d7ca8e63371f3ee403c435c
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-06-10 10:12:34 +0100

    Added Task-CPAN-SmokeBox

---
+++ Task-CPAN-Smokebox/lib/Task/CPAN/SmokeBox.pm
@@ -0,0 +1,35 @@
+package Task::CPAN::SmokeBox;
+
+use strict;
+use warnings;
+use vars qw[$VERSION];
+
+$VERSION = '0.02';
+
+qq[smoky smoke box foo]
+
+__END__
+
+=head1 NAME
+
+Task::CPAN::SmokeBox - Install the things to make a CPAN Testers smokebox
+
+=head1 SYNOPSIS
+
+
+
+=head1 DESCRIPTION
+
+Things
+
+=head1 AUTHOR
+
+Chris C<BinGOs> Williams <chris@bingosnet.co.uk>
+
+=head1 LICENSE
+
+Copyright E<copy> Chris Williams
+
+This module may be used, modified, and distributed under the same terms as Perl itself. Please see the license that came with your Perl distribution for details.
+
+=cut
