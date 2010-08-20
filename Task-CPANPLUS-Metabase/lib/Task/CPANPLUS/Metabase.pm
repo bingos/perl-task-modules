50de113dcc9a1cf63128f5c14bb11f9882f4d81b
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2010-08-20 22:46:44 +0100

    Added Task-CPANPLUS-Metabase

---
+++ Task-CPANPLUS-Metabase/lib/Task/CPANPLUS/Metabase.pm
@@ -0,0 +1,25 @@
+package Task::CPANPLUS::Metabase;
+
+# ABSTRACT: Install everything for CPANPLUS to use Metabase
+
+use strict;
+use warnings;
+
+1;
+
+=head1 SYNOPSIS
+
+  cpanp -i Task::CPANPLUS::Metabase
+
+  metabase-cpanp
+
+=head1 DESCRIPTION
+
+Task::CPANPLUS::Metabase is a L<Task> module that installs the modules
+required for using L<Test::Reporter::Transport::Metabase> with L<CPANPLUS>
+for submitting CPAN test reports to the L<Metabase>.
+
+Also included is L<metabase-cpanp> script which will generate an appropriate
+id file and configure L<CPANPLUS> for submitting CPAN test reports.
+
+=cut
