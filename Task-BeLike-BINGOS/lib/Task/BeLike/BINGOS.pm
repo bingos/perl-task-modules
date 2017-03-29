91281744b33c3235296d7ed0c6a0b1322ebce24a
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2017-03-29 19:07:31 +0100

    Switch BeLike to Dist::Zilla

--- Task-BeLike-BINGOS/lib/Task/BeLike/BINGOS.pm
+++ Task-BeLike-BINGOS/lib/Task/BeLike/BINGOS.pm
@@ -1,18 +1,13 @@
 package Task::BeLike::BINGOS;
 
+#ABSTRACT: Be the most BINGOS you can be
+
 use strict;
 use warnings;
-use vars qw($VERSION);
-
-$VERSION = '1.04';
 
 'Looks like I picked the wrong week to quit sniffing glue.'
 
-__END__
-
-=head1 NAME
-
-Task::BeLike::BINGOS - Be the most BINGOS you can be
+=pod
 
 =head1 DESCRIPTION
 
@@ -20,15 +15,5 @@ Task::BeLike::BINGOS is a L<Task> module to install all the modules that I like 
 kicking around.
 
 See the C<Makefile.PL> file in this distribution for details.
-
-=head1 AUTHOR
-
-Chris C<BinGOs> Williams <chris@bingosnet.co.uk>
-
-=head1 LICENSE
-
-Copyright E<copy> Chris Williams
-
-This module may be used, modified, and distributed under the same terms as Perl itself. Please see the license that came with your Perl distribution for details.
 
 =cut
